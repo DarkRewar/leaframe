f5ff7675e7d47662e482a3a28ac9d31f81325003
Curtis Pelissier <curtis.pelissier@gmail.com>
2025-01-17 16:18:42 +0100

    Add PieChart Data Hovering info

--- Runtime/Controls/Charts/AxesChart.cs
+++ Runtime/Controls/Charts/AxesChart.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace Leaframe.Controls.Charts
+namespace Leaframe.Charts
 {
     public abstract class AxesChart : Chart
     {
--- Runtime/Controls/Charts/LineChart.cs
+++ Runtime/Controls/Charts/LineChart.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
 using UnityEngine;
 using UnityEngine.Scripting;
 using UnityEngine.UIElements;
 
-namespace Leaframe.Controls.Charts
+namespace Leaframe.Charts
 {
     public class LineChart : AxesChart
     {
